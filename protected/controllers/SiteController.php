689879223706c80b464e5e5e927a51db93c1c92b
goloshchapov <nikita.goloshchapov@easternpeak.com>
2014-03-05 17:23:29 +0200

    new pages markup

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -30,7 +30,7 @@ class SiteController extends Controller
         $this->layout = '//layouts/default';
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
-		$this->render('test');
+		$this->render('opened class');
 	}
 
 	/**
