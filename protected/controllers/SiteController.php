14e3a9648903084cc25b1060bb6afbe0f82b8a59
goloshchapov <nikita.goloshchapov@easternpeak.com>
2014-03-11 17:35:13 +0200

    new

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -12,7 +12,7 @@ class SiteController extends Controller
         $this->layout = '//layouts/default';
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
-		$this->render('index');
+		$this->render('jobs');
 	}
 
 	/**
