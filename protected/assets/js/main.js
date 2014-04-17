f180b6df441f7224b5ac35af04aff7c7441db347
goloshchapov <nikita.goloshchapov@easternpeak.com>
2014-04-17 17:25:50 +0300

    last change

--- protected/assets/js/main.js
+++ protected/assets/js/main.js
@@ -205,3 +205,23 @@ $(document).ready(function () {
     $('.header-menu li a[href="'+link+'"]').parent().addClass('active');
 
 });
+/* tabs */
+
+$(document).ready(function() {
+
+
+    $(".tab_content").hide();
+    $("ul.tabs li:first").addClass("actived").show();
+    $(".tab_content:first").show();
+
+
+    $("ul.tabs li").click(function() {
+        $("ul.tabs li").removeClass("actived");
+        $(this).addClass("actived");
+        $(".tab_content").hide();
+        var activeTab = $(this).find("a").attr("href");
+        $(activeTab).fadeIn();
+        return false;
+    });
+
+});
