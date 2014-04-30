7ca08063a5da00f33b844801db77fbc1fbbeaf2c
nikita.podgorbunskyi <nikita.podgorbunskyi@easternpeak.com>
2014-04-30 18:33:02 +0300

    minor fix

--- protected/models/Seminar.php
+++ protected/models/Seminar.php
@@ -4,7 +4,41 @@ Yii::import('application.models._base.BaseSeminar');
 
 class Seminar extends BaseSeminar
 {
-	public static function model($className=__CLASS__) {
-		return parent::model($className);
-	}
+    const TYPE_SUMMER = 'summer';
+    const TYPE_TRIMESTER = 'trimester';
+    
+    
+    public static function model($className=__CLASS__) {
+            return parent::model($className);
+    }
+        
+        
+    public static function getTypes()
+    {
+        return [self::TYPE_SUMMER => 'summer', self::TYPE_TRIMESTER => 'trimester'];
+    }
+    
+    public function getGradesIDs()
+    {
+
+        return array_map(function ($item) {
+
+            return $item->id;
+        }, ($this->grades ?: []));
+    }
+
+    protected function afterSave(){
+        parent::afterSave();
+
+        foreach($this->times as $time) {
+            $time->seminar_id = $this->id;
+            $time->save();
+        }
+
+        foreach($this->datePeriods as $datePeriod) {
+
+            $datePeriod->seminar_id = $this->id;
+            $datePeriod->save();
+        }
+    }
 }
