f62cf8c2a35a2038f8c54980ee96d9fc7648a4bb
Eugene Steblin <eugene.steblin@easternpeak.com>
2014-05-08 19:17:51 +0300

    breadcrumbs on classes pages

--- protected/controllers/ClassesController.php
+++ protected/controllers/ClassesController.php
@@ -4,7 +4,7 @@ class ClassesController extends Controller
 {
 
     public $layout = '//layouts/default';
-
+/*
     public $breadcrumbs = [
         ['label' =>'Classes','url' => '/student/profile'],
         ['label' =>'Summer Seminars','url' => 'adadadadadads',
@@ -24,7 +24,7 @@ class ClassesController extends Controller
 
             ]
         ]
-    ];
+    ];*/
 
     /**
      * This is the default 'index' action that is invoked
@@ -47,11 +47,13 @@ class ClassesController extends Controller
 
     public function actionHillview()
     {
+        $this->breadcrumbs = BreadCrumbs::getBreadCrumbs('Classes/Writing Workouts at Hillview');
         $this->render("hillview");
     }
 
     public function actionOakknoll()
     {
+        $this->breadcrumbs = BreadCrumbs::getBreadCrumbs('Classes/Creative Writing at Oak Knoll');
         $this->render("knoll");
     }
     
@@ -61,37 +63,44 @@ class ClassesController extends Controller
         {
             case 'test_prep_boot_camp':
 
+                $this->breadcrumbs = BreadCrumbs::getBreadCrumbs('Classes/Summer Seminars/Test prep boot camp');
                 $this->render("details/test_prep_boot_camp");
                 
                 break;
 
             case 'essay_statement':
 
+                $this->breadcrumbs = BreadCrumbs::getBreadCrumbs('Classes/Summer Seminars/Crafting the personal essay statement');
                 $this->render("details/essay_statement");
                 
                 break;
             case 'myths_and_monsters':
 
+                $this->breadcrumbs = BreadCrumbs::getBreadCrumbs('Classes/Summer Seminars/Of myths and monsters');
                 $this->render("details/myths_and_monsters");
                 
                 break;
             case 'the_dogs':
 
+                $this->breadcrumbs = BreadCrumbs::getBreadCrumbs('Classes/Summer Seminars/Going to the dogs');
                 $this->render("details/the_dogs");
                 
                 break;
             case 'make_them_laugh':
 
+                $this->breadcrumbs = BreadCrumbs::getBreadCrumbs('Classes/Summer Seminars/Make‘em laugh');
                 $this->render("details/make_them_laugh");
                 
                 break;
             case 'power_of_story':
 
+                $this->breadcrumbs = BreadCrumbs::getBreadCrumbs('Classes/Summer Seminars/The power of story');
                 $this->render("details/power_of_story");
                 
                 break;
             case 'literary_analysis':
 
+                $this->breadcrumbs = BreadCrumbs::getBreadCrumbs('Classes/Summer Seminars/Intro to literary analysis');
                 $this->render("details/literary_analysis");
                 
                 break;
