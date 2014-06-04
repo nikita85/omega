dac2719d42bf64c52589c2a65fa37b68196ef284
Eugene Steblin <eugene.steblin@easternpeak.com>
2014-06-04 15:54:54 +0300

    js update

--- protected/assets/js/main.js
+++ protected/assets/js/main.js
@@ -1,14 +1,13 @@
 $(document).ready(function () {
 
 
-
-    $('[data-popup-open]').on('click', function(e){
+    $('[data-popup-open]').on('click', function (e) {
         e.preventDefault();
         $('#bg-layer').fadeIn(500);
         $('.' + $(this).attr('data-popup-open')).fadeIn(500);
     });
 
-    $('.popup-close').on('click', function(e){
+    $('.popup-close').on('click', function (e) {
         e.preventDefault();
         $('#bg-layer').fadeOut(500);
         $(this).parent().parent().fadeOut(500);
@@ -17,41 +16,37 @@ $(document).ready(function () {
     /* Apploicant Form */
     $('.cv_file_name').val('');
 
-    $('.applicant_back_to_form').on('click', function(e){
+    $('.applicant_back_to_form').on('click', function (e) {
         e.preventDefault();
         $('.applicant_form_success').hide();
         $('#applicant-form').show();
     });
 
 
-
     var link = window.location.pathname;
-    $('.header-menu li a[href="'+link+'"]').parent().addClass('active');
+    $('.header-menu li a[href="' + link + '"]').parent().addClass('active');
 
-});
-/* tabs */
-
-$(document).ready(function() {
+    /* tabs */
 
 
     $(".tab_content").hide();
     $("ul.tabs li:first").addClass("actived").show();
     $(".tab_content:first").show();
 
-     
-    $(".js-continue-button").click(function() {
-        
+
+    $(".js-continue-button").click(function () {
+
         $("li:has(a[href='#" + $(this).data("currentTab") + "'])").removeClass("actived");
         $("li:has(a[href='#" + $(this).data("nextTab") + "'])").addClass("actived");
 
         $(".tab_content").hide();
-        
+
         $(".tab_content#" + $(this).data("nextTab")).fadeIn();
-        
+
         return false;
     });
-    
-    $("ul.tabs li").click(function() {
+
+    $("ul.tabs li").click(function () {
         $("ul.tabs li").removeClass("actived");
         $(this).addClass("actived");
         $(".tab_content").hide();
